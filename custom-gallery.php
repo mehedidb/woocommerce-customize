1a5bc526b5cb0338f44e7fb8fc2682cb4e260b19
Mehedi Hasan Nahid <mehedidb@gmail.com>
2017-10-03 22:39:44 +0600

    Create custom-gallery.php

---
+++ custom-gallery.php
@@ -0,0 +1,31 @@
+// remove previouse image thumb.(reff on content-product.php)
+<?php remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
+
+
+
+//* Add gallery thumbs to woocommerce shop page
+<?php
+add_action('woocommerce_shop_loop_item_title','wps_add_extra_product_thumbs', 5);
+function wps_add_extra_product_thumbs() {
+
+	if ( is_shop() ) {
+
+		global $product;
+
+		$attachment_ids = $product->get_gallery_attachment_ids();
+
+		echo '<div class="product-thumbs">';
+
+		foreach( array_slice( $attachment_ids, 0,3 ) as $attachment_id ) {
+
+		  	$thumbnail_url = wp_get_attachment_image_src( $attachment_id,'default' );
+
+		  	echo '<img class="thumb" src="' . $thumbnail_url[0] . '">';
+
+		}
+
+		echo '</div>';
+	
+	}
+
+}
