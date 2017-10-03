624588f5e1643f657ce04aee2b603a19709a3254
Mehedi Hasan Nahid <mehedidb@gmail.com>
2017-10-03 11:29:46 +0600

    Update minicart_with_ajaxifay.php

--- minicart_with_ajaxifay.php
+++ minicart_with_ajaxifay.php
@@ -5,6 +5,8 @@
 // Ensure cart contents update when products are added to the cart via AJAX (place the following in functions.php).
 // Used in conjunction with https://gist.github.com/DanielSantoro/1d0dc206e242239624eb71b2636ab148
 // Compatible with 3.0.1+, for lower versions, remove "woocommerce_" from the first mention on Line 4
+
+<?php
 add_filter('woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment');
  
 function woocommerce_header_add_to_cart_fragment( $fragments ) {
