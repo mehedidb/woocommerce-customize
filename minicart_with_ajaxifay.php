a43b10677dc56142a961c71b9d0002cd9837696c
Mehedi Hasan Nahid <mehedidb@gmail.com>
2017-10-03 11:28:57 +0600

    Create minicart_with_ajaxifay.php

---
+++ minicart_with_ajaxifay.php
@@ -0,0 +1,23 @@
+#This is for showing product details content..
+<a class="cart-customlocation" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> - <?php echo WC()->cart->get_cart_total(); ?></a>
+
+// This is for ajaxify content. refference https://docs.woocommerce.com/document/show-cart-contents-total/
+// Ensure cart contents update when products are added to the cart via AJAX (place the following in functions.php).
+// Used in conjunction with https://gist.github.com/DanielSantoro/1d0dc206e242239624eb71b2636ab148
+// Compatible with 3.0.1+, for lower versions, remove "woocommerce_" from the first mention on Line 4
+add_filter('woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment');
+ 
+function woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+	
+	ob_start();
+	
+	?>
+	<a class="cart-customlocation" href="<?php echo $woocommerce->cart->get_cart_url(); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?> - <?php echo $woocommerce->cart->get_cart_total(); ?></a>
+	<?php
+	
+	$fragments['a.cart-customlocation'] = ob_get_clean();
+	
+	return $fragments;
+	
+}
